e6256d87807a579ee10e7e3e24a4a85317730f9e
fedebati <federicobatistella@gmail.com>
2022-07-25 11:02:46 -0300

    falta acomodar los botones externos

--- my-portfolio/src/Components/Bubble.js
+++ my-portfolio/src/Components/Bubble.js
@@ -9,13 +9,22 @@ function Bubble() {
     };
     
     return (
-        
-        <div className={(toggle ? 'blob' : 'blob active')} onClick={fnToggle}>
-            <button type='button'  className={(toggle? 'burger': 'burger active' )} >
-            <span className='line'></span>
-            <span className='line'></span>
-            <span className='line'></span>
-            </button>
+        <div className='containerBubble'>
+            <div className={(toggle ? 'blob' : 'blob active')} onClick={fnToggle}>
+                <button type='button'  className={(toggle? 'burger': 'burger active' )} >
+                <span className='line'></span>
+                <span className='line'></span>
+                <span className='line'></span>
+                </button>
+            </div>
+            <div className={(toggle ? 'loader' : 'loader active')} onClick={fnToggle}>
+                <span className='btn ' id='btn1'><h2>1</h2></span>
+                <span className='btn ' id='btn0'><h2>0</h2></span>
+                <span className='btn ' id='btn2'><h2>2</h2></span>
+                <span className='btn ' id='btn3'><h2>3</h2></span>
+                <span className='btn ' id='btn4'><h2>4</h2></span>
+                <span className='btn ' id='btn5'><h2>5</h2></span>
+            </div>
         </div>
     
     )
